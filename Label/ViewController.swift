ee0a9e1e94603798e05fb05365ef6505ae1a1a62
Mark Meretzky <mark.meretzky@gmail.com>
2020-04-01 20:20:58 -0400

    Initial Commit

---
+++ Label/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Label
+//
+//  Created by Mark Meretzky on 4/1/20.
+//  Copyright © 2020 New York University School of Professional Studies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
